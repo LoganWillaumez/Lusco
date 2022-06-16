6c79755626d4a36be8010260a89bc34479292420
LoganW <logan.willaumez@gmail.com>
2022-06-16 16:26:51 +0200

    [MERGE](redux-implementation): add redux logic and rtk queries

---
+++ app/api/getSearch.js
@@ -0,0 +1,25 @@
+import { emptySplitApi } from '../api/emptySplitApi';
+const searchApi = emptySplitApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getGoogleSearch: builder.mutation({
+      /**
+       * Query for login user. Take the email and password on parameter, and send it to the server.
+       * @param {*} param0
+       * @returns
+       */
+      query: (type, query, video) => {
+        return {
+          url: `${type}/q=${query} ${video && 'video'}`,
+          method: 'GET',
+          contentType: 'application/json',
+          headers: {
+            'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+            'x-rapidapi-key': process.env.NEXT_PUBLIC_KEY,
+          },
+        };
+      },
+    }),
+  }),
+});
+
+export const { useGetGoogleSearchMutation } = searchApi;
---
+++ app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { emptySplitApi } from './api/emptySplitApi';
+
+export const store = configureStore({
+  reducer: {
+    [emptySplitApi.reducerPath]: emptySplitApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(emptySplitApi.middleware),
+});
