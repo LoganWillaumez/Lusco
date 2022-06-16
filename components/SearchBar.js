4c70d25d69c7f806da52c2998584909b50b28c5c
LoganW <logan.willaumez@gmail.com>
2022-06-16 18:39:21 +0200

    [FEAT](searchBar): add forward animation

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,13 +1,30 @@
+import { useState } from 'react';
+
 export const SearchBar = () => {
+  const [isClick, setIsClick] = useState(false);
+  const toggleClick = () => {
+    setIsClick(!isClick);
+  };
+  console.log(`🚀 ~ isClick`, isClick);
   return (
-    <div className='flex flex-col items-center gap-1 md:gap-2 lg:gap-3 text-white'>
+    <div
+      className={`flex flex-col items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 gap-1 md:gap-2 lg:gap-3 text-white  ${
+        isClick && 'animate-mooveUp'
+      }`}
+    >
       <h1 className='text-[1.1rem] md:text-[1.3rem] lg:text-[1.5rem] font-bold'>
         Lusco
       </h1>
-      <h2 className='text-[0.6rem] md:text-[0.8rem] lg:text-[1rem]'>
+      <h2 className={`text-[0.6rem] md:text-[0.8rem] lg:text-[1rem] `}>
         Personnal project using google API and Next
       </h2>
-      <form className='flex  gap-3 items-center w-[80vw] max-w-[500px]'>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          toggleClick();
+        }}
+        className='flex  gap-3 items-center w-[80vw] max-w-[500px]'
+      >
         <input type='text' className='rounded w-full' />
         <button type='submit' className='w-10 h-6 bg-white rounded'>
           <i className='fa-solid fa-magnifying-glass text-gray-400' />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        // mooveUp: {
+        //   from: { transform: 'translateY(0vh)' },
+        //   '20%': { transform: 'translateY(6vh)' },
+        //   to: { transform: 'translateY(-35vh)' },
+        // },
+
+        //--
+        // absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+        //--
+        mooveUp: {
+          from: { top: '50%' },
+          '20%': { top: '60%' },
+          to: { top: '10%' },
+        },
+      },
+      animation: {
+        mooveUp: 'mooveUp 1s ease-in-out forwards',
+      },
+    },
   },
+
   plugins: [],
 };
